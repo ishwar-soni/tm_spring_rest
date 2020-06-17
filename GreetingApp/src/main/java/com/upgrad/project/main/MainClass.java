e1e5cabc1eee9c21c1b67887c1652b711cdc8b1a
satishmahajan <satish.mahajan1@gmail.com>
2020-06-17 22:24:48 +0530

    session 1 day 1 code commit

--- GreetingApp/src/main/java/com/upgrad/project/main/MainClass.java
+++ GreetingApp/src/main/java/com/upgrad/project/main/MainClass.java
@@ -1,15 +1,15 @@
 package com.upgrad.project.main;
-
 import com.upgrad.project.services.GreetingService;
+import com.upgrad.project.util.GreetingServiceFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainClass {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("projectBeans.xml");
 
-        GreetingService greetingService = (GreetingService) context.getBean("greetingService");
-        System.out.println( greetingService.sayHello("Satish","Mahajan"));
-        System.out.println(greetingService.sayHi("Satish","Mahajan"));
+        ApplicationContext context = new ClassPathXmlApplicationContext("projectBeans.xml");
+        GreetingService service = (GreetingService)context.getBean("greetingService");
+        System.out.println(service.sayHello("Satish","Mahajan"));
+
     }
 }
---
+++ GreetingApp/src/main/java/com/upgrad/project/util/GreetingServiceFactory.java
@@ -0,0 +1,16 @@
+package  com.upgrad.project.util;
+
+import com.upgrad.project.services.GreetingService;
+import com.upgrad.project.services.GreetingServiceImpl1;
+import com.upgrad.project.services.GreetingServiceImpl2;
+
+public class GreetingServiceFactory {
+
+    public GreetingService getGreetingService(String type) {
+        if (type.equals("1"))
+            return new GreetingServiceImpl1();
+        else if (type.equals("2"))
+            return new GreetingServiceImpl2();
+        return  null;
+    }
+}
